2343df23678b7fa65104cc040862e15ab890cca3
thangduc <tdthang.it@gmail.com>
2023-07-31 22:01:10 +0700

    update

--- plugins/api.js
+++ plugins/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: process.env.api || "http://api.besgroup.asia",
+  baseURL: "https://api.besgroup.asia",
 });
 
 export default axiosApi;
